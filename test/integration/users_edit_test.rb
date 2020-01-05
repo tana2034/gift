83450574a96c8df9fe8dd19cb390bdb3578f3488
tana2034 <2034tana@gmail.com>
2020-01-05 06:59:29 +0000

    #3 forward friendly after user's login

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,8 +18,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
-    log_in_as(@user)
     get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
