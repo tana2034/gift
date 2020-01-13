d5ed8daf3c5b481f39e0e4f29f2679de3d7f62f3
tana2034 <2034tana@gmail.com>
2020-01-13 07:12:49 +0000

    auto correct by rubocop

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,7 +1,8 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
-
   def setup
     @admin     = users(:michael)
     @non_admin = users(:archer)
@@ -12,12 +13,12 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     first_page_of_users = User.page(1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
+      assert_select "a[href=?]", user_path(user), text: user.name
       unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+        assert_select "a[href=?]", user_path(user), text: "delete"
       end
     end
-    assert_difference 'User.count', -1 do
+    assert_difference "User.count", -1 do
       delete user_path(@non_admin)
     end
   end
@@ -25,6 +26,6 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   test "index as non-admin" do
     log_in_as(@non_admin)
     get users_path
-    assert_select 'a', text: 'delete', count: 0
+    assert_select "a", text: "delete", count: 0
   end
 end
